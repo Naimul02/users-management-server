751fd216c0b85857f1c4811cc540e774771e4b8e
Naimul02 <naimulislum39@gmail.com>
2024-04-20 10:56:20 +0600

    commit no 1

---
+++ importent-comment.js
@@ -0,0 +1,9 @@
+//  "scripts": {
+//     //eta deyar karon holo : amra jokhon bibhinno jaygay deploy korbo sekhane nodemon thakte ooo pare abar na ooo thakte pare.jodi nodemon amra use kori nodemon er command ta jodi likhi taile dev dependency ba ekta dependency hisebe nodemon ta bosai dite hobe.tooo nodemon ta jehetu dependency hisebe dibo na tai normal node a dilam arki.ar index.js ta k calabe ei jonno amra start index.js ei command ta diye rakhbo.kothay deploy kori etar upore depend kore arki.    
+//  "start" : "node index.js",
+
+//  },
+
+
+
+//  ar entry point jehetu index.js sehetu amra index.js nam a ekta file toiri korbo.
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+//tumi jodi kono server a deploy koro 5000 port onekei use korte pare sobai re  ei port dite parbe na tokhon precess.env.PORT eta use korle deploy korar somoy tara nijeder moto port toiri kore nibe.ar eta korle amader deployment a kono somossa hobe na.
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+
+const users = [
+  { id: 1, name: "Sabana", email: "sabana@gmail.com" },
+  { id: 2, name: "Sabnoor", email: "Sabnoor@gmail.com" },
+  { id: 3, name: "Sabila", email: "Sabila@gmail.com" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Users Management server is running");
+});
+
+// egular nam api
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("post api hitting");
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT : ${port}`);
+});
